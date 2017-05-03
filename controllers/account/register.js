29c28eb366c999b37a85fcc9298b1f62245b1851
LS8 <lennartschoettker@hotmail.com>
2017-05-03 19:43:35 +0200

    construct a new Account object from the request and save it to the db

--- controllers/account/register.js
+++ controllers/account/register.js
@@ -1,3 +1,17 @@
+const Account = require('../../models/Account');
 module.exports = (req, res) => {
-  res.send('register controller works');
+  const newAccount = new Account({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  Account.addAccount(newAccount, (err, account) => {
+    if (err) {
+      res.json({ success: false, msg: 'Failed to register' });
+    } else {
+      res.json({ success: true, msg: 'User registered' });
+    }
+  });
 }
