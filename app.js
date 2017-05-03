a19c6fe69d17a976bcefd0a70a647a7754f165a7
LS8 <lennartschoettker@hotmail.com>
2017-05-03 19:42:43 +0200

    use db connection from config file and use body parser

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+const config = require('./config/database');
 
 // Routes
 const account = require('./routes/account');
 const userProfile = require('./routes/userProfile');
 
+
+// Connect To Database
+mongoose.connect(config.dbLocation);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to database ${config.dbLocation}`);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log(`Database error: ${err}`);
+});
+
 const app = express();
+
+app.use(bodyparser.json());
 
 // Use Routes
 app.use('/account', account);
