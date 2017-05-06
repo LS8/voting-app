49e822e66b00bb2ff6945f28cd15e9b1bf5a85cb
LS8 <lennartschoettker@hotmail.com>
2017-05-06 10:11:08 +0200

    add space to follow style guide

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,6 @@ const passport = require('passport');
 const Router = express.Router();
 const polls = require('../controllers/profile/polls');
 
-Router.get('/polls', passport.authenticate('jwt', { session: false}), polls);
+Router.get('/polls', passport.authenticate('jwt', { session: false }), polls);
 
 module.exports = Router;
