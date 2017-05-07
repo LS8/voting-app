62162714daf351b21b826c437102a9b5f3d0e89a
LS8 <lennartschoettker@hotmail.com>
2017-05-07 11:10:29 +0200

    add conditional

--- controllers/account/authenticate.js
+++ controllers/account/authenticate.js
@@ -5,38 +5,39 @@ const Account = require('../../models/Account');
 module.exports = (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  
+
   Account.getAccountByUsername(username, (err, account) => {
     if (err) {
       throw err;
     }
     if (!account) {
       res.json({ success: false, msg: 'Account not found' });
+    } else {
+      Account.checkPassword(password, account.password, (err, isMatch) => {
+        if (err) {
+          throw err;
+        }
+        // authentication successfull
+        if (isMatch) {
+          // create token
+          const token = jwt.sign(account, secret, {
+            expiresIn: 604800 // one Week
+          });
+          res.json({
+            success: true,
+            msg: 'Login successfull',
+            token: `JWT ${token}`,
+            account: {
+              id: account._id,
+              name: account.name,
+              username: account.username,
+              email: account.email
+            }
+          });
+        } else {
+          res.json({ success: false, msg: 'Wrong password' });
+        }
+      })
     }
-    Account.checkPassword(password, account.password, (err, isMatch) => {
-      if (err) {
-        throw err;
-      }
-      // authentication successfull
-      if (isMatch) {
-        // create token
-        const token = jwt.sign(account, secret, {
-          expiresIn: 604800 // one Week
-        });
-        res.json({
-          success: true,
-          msg: 'Login successfull',
-          token: `JWT ${token}`,
-          account: {
-            id: account._id,
-            name: account.name,
-            username: account.username,
-            email: account.email
-          }
-         });
-      } else {
-        res.json({ success: false, msg: 'Wrong password' });
-      }
-    })
   })
 };
