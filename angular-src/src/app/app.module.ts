df7cd69b701047d56744312f950f7117f2f817dc
LS8 <lennartschoettker@hotmail.com>
2017-05-08 20:35:47 +0200

    include router and  new components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
+import { Router } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    Router,
     MaterializeModule
   ],
   providers: [],
