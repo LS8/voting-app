1a53585fcb04bddfeae204775b9d98ac3516dc6c
LS8 <lennartschoettker@hotmail.com>
2017-05-09 21:16:28 +0200

    add status numbers to json response

--- controllers/account/register.js
+++ controllers/account/register.js
@@ -9,13 +9,13 @@ module.exports = (req, res) => {
 
   Account.addAccount(newAccount, (err, account, affectedRows, alreadyRegistered) => {
     if (err) {
-      res.json({ success: false, msg: 'Failed to register' });
+      res.json({ success: false, msg: 'Failed to register', status: 1 });
     } else if (alreadyRegistered) {
-      res.json({ success: false, msg: 'Username already exists' });
+      res.json({ success: false, msg: 'Username already exists', status: 2 });
     } else if (account) {
-      res.json({ success: true, msg: 'Account registered' });
+      res.json({ success: true, msg: 'Account registered', status: 0 });
     } else {
-      res.json({ success: false, msg: 'Oops, something went wrong..' });
+      res.json({ success: false, msg: 'Oops, something went wrong..', status: 3 });
     }
   });
 }
