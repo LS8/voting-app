2f8d3f301196a77f9941e0514748609a98bd7386
LS8 <lennartschoettker@hotmail.com>
2017-05-09 16:47:37 +0200

    add validate service to check that no input fields are empty and validate email against a regexp

---
+++ angular-src/src/app/services/validate.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ValidateService {
+
+  constructor() { }
+
+  validateRegister(account) {
+    if (account.username == undefined || account.password == undefined || account.email == undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+}
