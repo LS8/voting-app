381b3c671973a882f0ab6bd23d1c9db91f5d1952
LS8 <lennartschoettker@hotmail.com>
2017-05-09 16:46:51 +0200

    link the components input fields to the class properties // add function to handle submit // use service to check if inputs are valid and use helper function to change the email input field's styling if it doesn't pass the regex

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  username: string;
+  password: string;
+  email: string;
 
-  constructor() { }
+  validEmail: boolean;
+
+
+  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  isValid(value: boolean) {
+    if (value == undefined) return;
+    return value ? "custom-valid" : "custom-invalid";
+  }
+
+  onRegisterSubmit() {
+    const account = {
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    // Required Fields
+    if (!this.validateService.validateRegister(account)) {
+      this.flashMessage.show('Please fill in all fields', { cssClass: 'alert alert-danger', timeout: 3000 })
+      return false;
+    }
+
+    // Validate Email
+    if (!this.validateService.validateEmail(this.email)) {
+      this.validEmail = false;
+      this.flashMessage.show('Please use a valid email address', { cssClass: 'alert alert-danger', timeout: 3000 })
+      return false;
+    }
   }
 
 }
