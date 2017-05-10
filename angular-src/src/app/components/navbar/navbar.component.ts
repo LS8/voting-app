7a4b5e51814647336dffff8435eca63a87fc321e
LS8 <lennartschoettker@hotmail.com>
2017-05-10 11:30:42 +0200

    add click event handler for the logout

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
+  }
+
+  onLogoutClick() {
+    this.auth.logout();
+    this.flashMessage.show('You are now logged out', { cssClass: 'alert alert-success', timeout: 3000 })
+    this.router.navigate(['/']);
+    return false;
   }
 
 }
