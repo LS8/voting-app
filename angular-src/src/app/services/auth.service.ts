b3023b0536f683093d70f55c9c72e87d16c8d2f9
LS8 <lennartschoettker@hotmail.com>
2017-05-10 11:31:27 +0200

    add account authentication and storage // add logout

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -9,11 +9,31 @@ export class AuthService {
 
   constructor(private http: Http) { }
 
+  authenticateAccount(account) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/account/authenticate', account, { headers: headers })
+      .map(res => res.json());
+  }
+
   registerAccount(account) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/account/register', account, { headers: headers })
       .map(res => res.json());
+  }
+
+  storeAccountData(token, account) {
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('account', JSON.stringify(account));
+    this.authToken = token;
+    this.account = account;
+  }
+
+  logout() {
+    this.authToken = null;
+    this.account = null;
+    localStorage.clear();
   }
 
 }
