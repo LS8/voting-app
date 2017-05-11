b5cbddeae76c20aaf39ad2aed9a644ef8093dd9a
LS8 <lennartschoettker@hotmail.com>
2017-05-11 08:56:57 +0200

    add method that gets the profile (fetches polls by the user logged in) // add method to load the token from the local storage // add method that verifies if the user is logged in (token not expired)

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { tokenNotExpired } from 'angular2-jwt';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -14,6 +15,25 @@ export class AuthService {
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/account/authenticate', account, { headers: headers })
       .map(res => res.json());
+  }
+
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/profile/', { headers: headers })
+      .map(res => res.json());
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    this.loadToken();
+    return tokenNotExpired('id_token');
   }
 
   registerAccount(account) {
