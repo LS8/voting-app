4ed0a4871006935263f57eb28be9d1835686d382
LS8 <lennartschoettker@hotmail.com>
2017-05-14 10:59:14 +0200

    remove authorization for fetching individual polls

--- angular-src/src/app/services/poll.service.ts
+++ angular-src/src/app/services/poll.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
-import { tokenNotExpired } from 'angular2-jwt';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -11,20 +10,8 @@ export class PollService {
 
   getIndividualPoll(id) {
     let headers = new Headers();
-    this.loadToken();
-    headers.append('Authorization', this.authToken);
     headers.append('Content-Type', 'application/json');
     return this.http.get(`http://localhost:3000/polls/${id}`, { headers: headers })
       .map(res => res.json());
-  }
-
-  loadToken() {
-    const token = localStorage.getItem('id_token');
-    this.authToken = token;
-  }
-
-  loggedIn() {
-    this.loadToken();
-    return tokenNotExpired('id_token');
   }
 }
