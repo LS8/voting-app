6c503a645b9e91f589cec90e175c05886be89861
LS8 <lennartschoettker@hotmail.com>
2017-05-14 10:57:16 +0200

    use service to get the poll that corresponds to the url parameter

---
+++ angular-src/src/app/components/poll-detail/poll-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { PollService } from '../../services/poll.service';
+
+@Component({
+  selector: 'app-poll',
+  templateUrl: './poll-detail.component.html',
+  styleUrls: ['./poll-detail.component.css']
+})
+export class PollDetailComponent implements OnInit {
+  pollId: string;
+  pollToDisplay;
+
+  constructor(
+    private pollService: PollService,
+    private location: Location,
+    private route: ActivatedRoute
+    ) { }
+
+  ngOnInit() {
+    this.route.params.forEach(urlParameters => {
+      this.pollId = urlParameters["id"];
+    });
+    this.pollService.getIndividualPoll(this.pollId).subscribe(res => {
+      this.pollToDisplay = res.poll;
+      console.log(this.pollToDisplay);
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
+  }
+
+}
