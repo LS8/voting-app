823b4ce04ad1bda959b1b70b03f6d22e1391b74d
LS8 <lennartschoettker@hotmail.com>
2017-05-18 10:27:17 +0200

    add post route for voting

--- routes/polls.js
+++ routes/polls.js
@@ -2,9 +2,12 @@ const express = require('express');
 const Router = express.Router();
 const allPolls = require('../controllers/polls/all');
 const individualPoll = require('../controllers/polls/individual');
+const vote = require('../controllers/polls/vote');
 
 Router.get('/', allPolls);
 
 Router.get('/:id', individualPoll);
+
+Router.post('/:id', vote);
 
 module.exports = Router;
