67ec5ffbbe715a77afc25244ba1038a0818fa7a3
LS8 <lennartschoettker@hotmail.com>
2017-05-19 12:31:43 +0200

    add ip route that uses the ip controller

--- routes/account.js
+++ routes/account.js
@@ -4,9 +4,12 @@ const Router = express.Router();
 // Controllers
 const register = require('../controllers/account/register');
 const authenticate = require('../controllers/account/authenticate');
+const ip = require('../controllers/account/ip');
 
 Router.post('/register', register);
 
 Router.post('/authenticate', authenticate);
+
+Router.get('/ip', ip);
 
 module.exports = Router;
