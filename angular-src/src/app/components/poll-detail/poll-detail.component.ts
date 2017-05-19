5c2e785bc7c4d0662df0fa5c5ec374b041a7e94a
LS8 <lennartschoettker@hotmail.com>
2017-05-19 12:35:05 +0200

    load users (localstorage or ip) // add users to the already voted array in the poll when they vote// sync changes with the backend

--- angular-src/src/app/components/poll-detail/poll-detail.component.ts
+++ angular-src/src/app/components/poll-detail/poll-detail.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { PollService } from '../../services/poll.service';
+import { AuthService } from '../../services/auth.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
@@ -14,9 +15,12 @@ export class PollDetailComponent implements OnInit {
   pollId: string;
   selectedOption;
   pollToDisplay;
+  alreadyVoted: boolean;
+  user;
 
   constructor(
     private pollService: PollService,
+    private authService: AuthService,
     private flashMessage: FlashMessagesService,
     private location: Location,
     private router: Router,
@@ -29,6 +33,7 @@ export class PollDetailComponent implements OnInit {
     });
     this.pollService.getIndividualPoll(this.pollId).subscribe(res => {
       this.pollToDisplay = res.poll;
+      this.checkAlreadyVoted();
     },
     err => {
       console.log(err);
@@ -36,10 +41,45 @@ export class PollDetailComponent implements OnInit {
     });
   }
 
+  loadUser(callback) {
+    // if user is logged in
+    if (localStorage.getItem('account')) {
+      this.user = JSON.parse(localStorage.getItem('account')).username;
+      return callback();
+    } else {
+      // get and store ip
+      this.authService.loadIp().subscribe(res => {
+        this.user = res.ipAddress;
+        return callback();
+      });
+    }
+  }
+
+  checkAlreadyVoted() {
+    this.loadUser(() => {
+      // user hasn't voted yet
+      if (this.pollToDisplay.alreadyVoted.indexOf(this.user) === -1) {
+        this.alreadyVoted = false;
+        return false;
+      } else {
+        // user has already voted
+        this.alreadyVoted = true;
+        return true;
+      }
+    });
+    }
+
   onVoteSubmit() {
     if (this.selectedOption >= 0) {
+      // add vote and replace array with itself to force rerender
       this.pollToDisplay.votes[this.selectedOption] += 1;
       this.pollToDisplay.votes = this.pollToDisplay.votes.slice();
+
+      // add the person that voted
+      this.pollToDisplay.alreadyVoted.push(this.user);
+      this.alreadyVoted = true;
+
+      // sync poll with the poll stored in the db
       this.pollService.syncVote(this.pollToDisplay._id, {updatedPoll: this.pollToDisplay}).subscribe(res => {
         if (res.success) {
           this.flashMessage.show(`Your vote (${this.pollToDisplay.labels[this.selectedOption]}) has been added`, { cssClass: 'alert alert-success', timeout: 3000 });
@@ -50,5 +90,6 @@ export class PollDetailComponent implements OnInit {
       return false;
     }
   }
+
 
 }
