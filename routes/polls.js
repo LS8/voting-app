b6378ec0aa7bd9cd69e0199654241c6f8eba22cf
LS8 <lennartschoettker@hotmail.com>
2017-05-20 13:45:42 +0200

    add delete route that uses the delete controller

--- routes/polls.js
+++ routes/polls.js
@@ -3,11 +3,14 @@ const Router = express.Router();
 const allPolls = require('../controllers/polls/all');
 const individualPoll = require('../controllers/polls/individual');
 const vote = require('../controllers/polls/vote');
+const deletePoll = require('../controllers/polls/delete');
 
 Router.get('/', allPolls);
 
 Router.get('/:id', individualPoll);
 
 Router.post('/:id', vote);
+
+Router.delete('/:id', deletePoll);
 
 module.exports = Router;
