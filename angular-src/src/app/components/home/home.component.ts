1acf956b7379c4613a41e55ec268d2c20d9671e0
LS8 <lennartschoettker@hotmail.com>
2017-05-27 18:56:36 +0200

    add home component

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PollService } from '../../services/poll.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  polls: object[];
 
-  constructor() { }
+  constructor(
+    private pollService: PollService,
+    public authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.pollService.getAllPolls().subscribe(res => {
+      this.polls = res.polls;
+    });
+  }
+
+  goToDetailPage(clickedPoll) {
+    this.router.navigate(['polls', clickedPoll._id]);
   }
 
 }
